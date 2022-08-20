20bc15faa7c1a425e59d01c67dbb0dcab172b942
Araf-Khatri <arafkhatri0@gmail.com>
2022-08-21 00:03:40 +0530

    no changes ig

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
@@ -7,7 +6,6 @@ import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
-  
   const showCartHandler = () => {
     setCartIsShown(true);
   };
