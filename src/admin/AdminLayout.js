16bfaa642ea667aedb654ed3407a2a21f93fba05
Araf-Khatri <arafkhatri0@gmail.com>
2025-12-11 08:48:34 +0530

    added active tab on admin dashboard

--- src/admin/AdminLayout.js
+++ src/admin/AdminLayout.js
@@ -1,21 +1,29 @@
-import { Link, Outlet } from "react-router";
+import { Link, Outlet, useLocation } from "react-router";
 import "./AdminLayout.css";
 
+const adminNavigationLinks = [
+  { label: "Restaurants", path: "/admin/restaurants" },
+  { label: "Orders", path: "/admin/orders" },
+];
 const AdminLayout = () => {
+  const location = useLocation();
+
   return (
     <div className="admin-navbar-wrapper">
       <nav className="admin-navbar-nav">
         <ul className="admin-navbar-list">
-          <li className="admin-navbar-item">
-            <Link className="admin-navbar-link" to="/admin/restaurants">
-              Restaurants
-            </Link>
-          </li>
-          <li className="admin-navbar-item">
-            <Link className="admin-navbar-link" to="/admin/orders">
-              Orders
-            </Link>
-          </li>
+          {adminNavigationLinks.map((navLink) => (
+            <li className="admin-navbar-item">
+              <Link
+                className={`admin-navbar-link ${
+                  location.pathname.startsWith(navLink.path) ? "active" : ""
+                }`}
+                to={navLink.path}
+              >
+                {navLink.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
       <main className="admin-navbar-main">
